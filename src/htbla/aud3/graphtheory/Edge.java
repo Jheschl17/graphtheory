a9becdd2ec7425766f1c251b0acd50ce31162c9e
Emil <e.eichsteininger@gmx.net>
2020-11-06 11:02:30 +0100

    fuck

--- src/htbla/aud3/graphtheory/Edge.java
+++ src/htbla/aud3/graphtheory/Edge.java
@@ -4,7 +4,16 @@ package htbla.aud3.graphtheory;
  * @author TODO Bitte Gruppenmitglieder eintragen!
  */
 public class Edge {
-    
+
+    private Graph.Node from;
+    private Graph.Node to;
+    private int weight;
+    public Edge(Graph.Node from, Graph.Node to, int weight)
+    {
+        this.from=from;
+        this.to = to;
+        this.weight=weight;
+    }
     public int getFromNodeId() {
         return -1;
     }
--- src/htbla/aud3/graphtheory/Graph.java
+++ src/htbla/aud3/graphtheory/Graph.java
@@ -1,15 +1,64 @@
 package htbla.aud3.graphtheory;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
- * @author TODO Bitte Gruppenmitglieder eintragen!
+ * @author Jonas Heschl, Jakob Jodlbauer, Emil Eichsteininger
  */
 public class Graph {
-
+    public class Node
+    {
+        protected String name;
+        protected List<Edge> connections;
+        public Node(String name, List<Edge> connections)
+        {
+            this.name=name;
+            this.connections=connections;
+        }
+        public void setConnections(List<Edge> connections)
+        {
+            this.connections = connections;
+        }
+    }
+    Node[] nodes = new Node[50];
     public void read(File adjacencyMatrix) {
-	
+        Scanner s = null;
+        try {
+            s = new Scanner(adjacencyMatrix);
+        } catch (Exception e) {}
+        s.useDelimiter("\n");
+        int rep = 0;
+        for (int i = 0; i < 50; i++) {
+            String name = "";
+            for (int j = rep; j < rep+1; j++) {
+                if(i>25)
+                {
+                    char c = (char) (40+i-1);
+                    name ="A"+c;
+                }
+                else
+                {
+                    char c = (char)(i+65);
+                    name = ""+c;
+                }
+                rep=(i/25);
+            }
+            Node n = new Node(name, null);
+            nodes[i]=n;
+        }
+       /* while(s.hasNext())
+        {
+            String line = s.nextLine();
+            String[] parts = line.split(";");
+            List<Edge> edges = new ArrayList<>();
+        }*/
+        for (int i = 0; i < nodes.length; i++) {
+            System.out.println(nodes[i].name);
+        }
     }
     
     public Path determineShortestPath(int sourceNodeId, int targetNodeId) {
