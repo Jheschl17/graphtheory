fc4e26eeb81a1f3772a2708f927b5867698559c4
Jonas <heschl@protonmail.com>
2020-12-04 07:57:30 +0100

    Implement Graph::determineShortestPath

--- src/htbla/aud3/graphtheory/Graph.java
+++ src/htbla/aud3/graphtheory/Graph.java
@@ -1,5 +1,7 @@
 package htbla.aud3.graphtheory;
 
+import org.w3c.dom.Node;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -30,9 +32,81 @@ public class Graph {
             e.printStackTrace();
         }
     }
-    
+
+    private class SearchItem implements Comparable<SearchItem> {
+        public int id;
+        public int via = -1;
+        public int distance = Integer.MAX_VALUE;
+
+        public SearchItem(int id) {
+            this.id = id;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            SearchItem that = (SearchItem) o;
+            return id == that.id && via == that.via && Double.compare(that.distance, distance) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, via, distance);
+        }
+
+        @Override
+        public int compareTo(SearchItem o) {
+            return this.distance - o.distance;
+        }
+    }
+
     public Path determineShortestPath(int sourceNodeId, int targetNodeId) {
-        return null;
+        List<SearchItem> lst = allNodeIds().stream()
+                .map(SearchItem::new)
+                .collect(Collectors.toList());
+
+        Map<Integer, SearchItem> map = lst.stream()
+                .collect(Collectors.toMap(x -> x.id, x -> x));
+
+        // Set distance of starting point to zero and visited to true
+        map.get(sourceNodeId).distance = 0;
+
+        PriorityQueue<SearchItem> pq = new PriorityQueue<>(lst);
+
+        // Determine path using Dijkstra
+        while (pq.size() > 0) {
+            SearchItem cur = pq.remove();
+            for (Edge edge : edgesFrom(cur.id)) {
+                SearchItem si = map.get(edge.getToNodeId());
+
+                int alt = cur.distance + weight(cur.id, si.id);
+                if (alt < si.distance) {
+                    si.distance = alt;
+                    si.via = cur.id;
+                    pq.remove(si);
+                    pq.add(si);
+                }
+            }
+        }
+
+        // Retrieve path from data structure
+        return retrievePath(map, targetNodeId, this);
+    }
+
+    private static Path retrievePath(Map<Integer, SearchItem> map, int targetNodeId, Graph graph) {
+        List<Integer> ids = new ArrayList<>();
+        for (int id = targetNodeId; id != -1;) {
+            SearchItem item = map.get(id);
+            ids.add(item.id);
+            id = item.via;
+        }
+        Collections.reverse(ids);
+
+        int[] pathIds = ids.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+        return new Path(graph, pathIds);
     }
     
     public Path determineShortestPath(int sourceNodeId, int targetNodeId, int... viaNodeIds) {
@@ -66,9 +140,20 @@ public class Graph {
                         edge.getFromNodeId(),
                         edge.getToNodeId()
                 )))
-                .flatMapToInt(list -> (IntStream) list.stream())
+                .flatMap(list -> list.stream())
                 .distinct()
-                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Returns a list of all edges originating at the given node.
+     *
+     * @param fromNodeId The id of the node to search originating edges from.
+     * @return All edges originating from `fromNodeId`.
+     */
+    public List<Edge> edgesFrom(int fromNodeId) {
+        return edges.stream()
+                .filter(edge -> edge.getFromNodeId() == fromNodeId)
                 .collect(Collectors.toList());
     }
 
